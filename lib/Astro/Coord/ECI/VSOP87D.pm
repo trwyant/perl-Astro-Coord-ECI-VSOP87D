1e52bd5d4e01c292ea3a0bef425ed653aa1ad7f7
Tom Wyant <wyant@cpan.org>
2020-07-24 09:41:54 -0400

    Version 0.002_02
    Set version to 0.002_02. Update Changes file for changes since v00.002_01.

--- lib/Astro/Coord/ECI/VSOP87D.pm
+++ lib/Astro/Coord/ECI/VSOP87D.pm
@@ -28,7 +28,7 @@ BEGIN {
 	or constant->import( DEBUG => 0 );
 }
 
-our $VERSION = '0.002_01';
+our $VERSION = '0.002_02';
 
 my @basic_export = qw{
     SUN_CLASS
--- lib/Astro/Coord/ECI/VSOP87D/Sun.pm
+++ lib/Astro/Coord/ECI/VSOP87D/Sun.pm
@@ -13,7 +13,7 @@ use Astro::Coord::ECI::VSOP87D qw{ :sun };
 
 use Carp;
 
-our $VERSION = '0.002_01';
+our $VERSION = '0.002_02';
 
 sub new {
     my ( $class, %arg ) = @_;
