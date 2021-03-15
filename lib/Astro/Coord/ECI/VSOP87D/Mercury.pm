4c4db9edc80c5a43ef5276d31a99abb845754c59
Tom Wyant <wyant@cpan.org>
2021-03-14 22:56:43 -0400

    Version 0.005
    Set version to 0.005. Update Changes file for changes since v00.004_01.

--- lib/Astro/Coord/ECI/VSOP87D/Mercury.pm
+++ lib/Astro/Coord/ECI/VSOP87D/Mercury.pm
@@ -7,7 +7,7 @@ use warnings;
 
 use base qw{ Astro::Coord::ECI::VSOP87D::_Inferior };
 
-our $VERSION = '0.004_01';
+our $VERSION = '0.005';
 
 sub __model_definition {
     my ( undef, $key ) = @_;
